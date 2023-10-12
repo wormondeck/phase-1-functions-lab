8814d73ffdd113800f994c1778679ded6c716a7e
Ronney Arias <ronneyarias@gmail.com>
2023-10-12 13:28:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    
+     const headquartersLocation = 42;
+    
+     const distance = pickUpLocation - headquartersLocation;
+    
+     return Math.abs(distance);
+ }
+
+ function distanceFromHqInFeet(pickUpLocation) {
+    
+     const headquartersLocation = 42;
+
+     const distance = pickUpLocation - headquartersLocation;
+ 
+     return Math.abs(distance) * 264;
+    
+ }
+
+ function distanceTravelledInFeet(start, destination) {
+     const headquartersLocation = start;
+    
+     const pickUpLocation = destination;
+    
+     const distance = pickUpLocation - headquartersLocation;
+    
+     return Math.abs(distance) * 264;
+ }
+
+function calculatesFarePrice(start, destination) {
+         const distance = Math.abs(destination - start) * 264;
+      
+         if (distance <= 400) {
+           return 0;
+         }
+         if (distance >= 400 && distance <= 2000) {
+           return ((distance - 400) * 0.02);
+         }
+         if (distance > 2000 && distance <= 2500){
+           return 25;
+         }
+         if (distance >= 2500 ) {
+             return 'cannot travel that far';
+         }
+
+ }
+
